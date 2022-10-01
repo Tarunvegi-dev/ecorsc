ee0c40d025e6e2492297bc6a8a6c324abc281a89
TarunBabu <vegitarun1234@gmail.com>
2022-10-01 16:28:14 +0530

    admin pannel

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,10 +1,101 @@
 import React from 'react';
 import Adminlogin from './login';
+import { Form, Button, Col, Image } from 'react-bootstrap';
+import logo from '../../assets/logo.png'
+import { FaUpload } from 'react-icons/fa'
+import { FiLogOut } from 'react-icons/fi'
+import { RiSendPlaneFill } from 'react-icons/ri'
 
-const Admin = () => {
+const Admin = (props) => {
+    const logout = () => {
+        localStorage.removeItem('admin-username')
+        props.history.push('/admin')
+    }
+
     return (
-        <div className='container admin'>
-            <Adminlogin />
+        <div className='admin'>
+            {localStorage.getItem('admin-username') === process.env.REACT_APP_ADMIN_USERNAME
+                ?
+                <>
+                    <Button variant='danger' onClick={logout} style={{ float: 'right', margin: '20px', padding: '10px 15px' }}><FiLogOut size="18" />&nbsp;LOGOUT</Button>
+                    <div className='container'>
+                        <div>
+                            <center>
+                                <Image src={logo} height="120px" width="120px" style={{ marginTop: '30px' }} />
+                                <h2 className='heading'>ECoRSC Admin Dashboard</h2>
+                            </center>
+                        </div>
+                        <div className='admin row' style={{ marginTop: '60px' }}>
+                            <Col className='circulars' xs={12} sm={6}>
+                                <h4 className='circulars-title'>Upload Circulars</h4>
+                                <span>Fill the below form and you can upload circulars directly from here</span>
+                                <Form style={{ marginTop: '40px', fontFamily: 'Poppins' }}>
+                                    <div class="group">
+                                        <input type="text" required />
+                                        <span class="highlight"></span>
+                                        <span class="bar"></span>
+                                        <label>Title</label>
+                                    </div>
+                                    <div class="select">
+                                        <select class="select-text" required>
+                                            <option value="" disabled selected></option>
+                                            <option value="1">Railway Circulars</option>
+                                            <option value="2">Zonal PNM Minutes</option>
+                                        </select>
+                                        <span class="select-highlight"></span>
+                                        <span class="select-bar"></span>
+                                        <label class="select-label">Category</label>
+                                    </div><br /><br />
+                                    <div class="group">
+                                        <input type="text" required />
+                                        <span class="highlight"></span>
+                                        <span class="bar"></span>
+                                        <label>Pdf Link</label>
+                                    </div>
+                                    <div class="group">
+                                        <input class="form-control" type="file" id="formFile" />
+                                    </div>
+                                    <Button className='submitBtn'><FaUpload />&nbsp;UPLOAD</Button>
+                                </Form>
+                            </Col>
+                            <Col className='bulkSMS' xs={12} sm={6}>
+                                <h4 className='bulkSMSTitle'>Send Bulk SMS</h4>
+                                <span>Fill the below form and you can send bulk SMS directly from here</span>
+                                <Form style={{ marginTop: '40px', fontFamily: 'Poppins' }}>
+                                    <div class="group">
+                                        <textarea type="textarea" rows="5" required="required"></textarea><span class="highlight"></span><span class="bar"></span>
+                                        <label>Message</label>
+                                    </div>
+                                    <div class="select">
+                                        <select class="select-text" required>
+                                            <option value="" disabled selected></option>
+                                            <option value="1">Waltair</option>
+                                            <option value="2">Khurda</option>
+                                            <option value="3">Sambalpur</option>
+                                        </select>
+                                        <span class="select-highlight"></span>
+                                        <span class="select-bar"></span>
+                                        <label class="select-label">Divison</label>
+                                    </div><br /><br />
+                                    <div class="select">
+                                        <select class="select-text" required>
+                                            <option value="" disabled selected></option>
+                                            <option value="1">Waltair</option>
+                                            <option value="2">Khurda</option>
+                                            <option value="3">Sambalpur</option>
+                                        </select>
+                                        <span class="select-highlight"></span>
+                                        <span class="select-bar"></span>
+                                        <label class="select-label">Branch</label>
+                                    </div><br /><br />
+                                    <Button className='submitBtn'><RiSendPlaneFill />&nbsp;SEND</Button>
+                                </Form>
+                            </Col>
+                        </div>
+                    </div>
+                </>
+                : <Adminlogin />
+            }
         </div>
     );
 };
--- src/pages/Admin/login.js
+++ src/pages/Admin/login.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../assets/logo.png'
 import { Form, Image, Col, Button } from 'react-bootstrap'
 import './styles.css'
+import { withRouter } from 'react-router-dom';
 
-const Adminlogin = () => {
+const Adminlogin = (props) => {
+    const [username, setusername] = useState('');
+    const [password, setpassword] = useState('');
+    const [error, seterror] = useState('');
+
+    const loginUser = () => {
+        if (username === '') {
+            seterror('Please enter username')
+        } else if (password === '') {
+            seterror('Please enter password')
+        } else if (username !== process.env.REACT_APP_ADMIN_USERNAME || password !== process.env.REACT_APP_ADMIN_PASSWORD) {
+            seterror('Incorrect username/password')
+        } else {
+            localStorage.setItem('admin-username', username)
+            props.history.push('/admin')
+        }
+    }
+
     return (
         <div className='container login'>
             <center>
@@ -11,16 +29,17 @@ const Adminlogin = () => {
                 <h3>ECoRSC Admin Login</h3>
                 <div style={{ marginTop: '40px' }}>
                     <Form.Group as={Col} sm={4} >
-                        <Form.Control className="username-input" type='text' placeholder='Enter username' />
+                        <Form.Control className="username-input" type='text' placeholder='Enter username' value={username} onChange={(e) => setusername(e.target.value)} />
                     </Form.Group><br />
                     <Form.Group as={Col} sm={4} >
-                        <Form.Control className="username-input" type='password' placeholder='Enter password' />
+                        <Form.Control className="username-input" type='password' placeholder='Enter password' value={password} onChange={(e) => setpassword(e.target.value)} />
                     </Form.Group>
+                    <p style={{ color: 'red', marginTop: '20px' }}>{error}</p>
                 </div>
-                <Button variant="success" style={{ marginTop: '30px', padding: '5px 25px', fontSize: '17px' }}>Login</Button>
+                <Button variant="success" style={{ marginTop: '10px', padding: '5px 25px', fontSize: '17px' }} onClick={loginUser}>Login</Button>
             </center>
         </div>
     );
 };
 
-export default Adminlogin;
+export default withRouter(Adminlogin);

0759e97751a9072c6fd892b55c052934843219eb
devaraj764 <devaraju.edgroom.com>
2022-10-11 11:28:55 +0530

    Changes

--- src/pages/homepage/Circulars.js
+++ src/pages/homepage/Circulars.js
@@ -1,44 +1,44 @@
 import React from 'react';
 import { Row } from 'react-bootstrap';
-import { FaCalendarAlt } from 'react-icons/fa'
+import NewGif from '../../assets/new.gif'
 
 const Circulars = () => {
   return (
     <div className="Cirsulars">
-        <h3 className="title">Railway Board Circulars</h3>
-        <Events />
-        <br/>
-        <h3 className="title">ECoRSC PNM Items</h3>
-        <Events />
+      <h3 className="title">Railway Board Circulars</h3>
+      <Events />
+      <br />
+      <h3 className="title">ECoRSC PNM Items</h3>
+      <Events />
     </div >
   )
 }
 
 const Events = () => {
   return (
-    <div className="table">
-      <div className="item">
+    <ul className="table">
+      <li className="item">
+        <p>29th National Convention of NFIR to be held on 17th & 18th September 2019 at Ujjain
+          <img src={NewGif} height="28px" alt="New Gif" />
+        </p>
+      </li>
+      <li className="item">
         <p>29th National Convention of NFIR to be held on 17th & 18th September 2019 at Ujjain</p>
-        <FaCalendarAlt size={16} /> &nbsp; <i>Feb 15, 2018</i>
-      </div>
-      <div className="item">
+      </li>
+      <li className="item">
+        <p>29th National Convention of NFIR to be held on 17th & 18th September 2019 at Ujjain
+          <img src={NewGif} height="28px" alt="New Gif" />
+        </p>
+      </li>
+      <li className="item">
         <p>29th National Convention of NFIR to be held on 17th & 18th September 2019 at Ujjain</p>
-        <FaCalendarAlt size={16} /> &nbsp; <i>Feb 15, 2018</i>
-      </div>
-      <div className="item">
-        <p>29th National Convention of NFIR to be held on 17th & 18th September 2019 at Ujjain</p>
-        <FaCalendarAlt size={16} /> &nbsp; <i>Feb 15, 2018</i>
-      </div>
-      <div className="item">
-        <p>29th National Convention of NFIR to be held on 17th & 18th September 2019 at Ujjain</p>
-        <FaCalendarAlt size={16} /> &nbsp; <i>Feb 15, 2018</i>
-      </div>
+      </li>
 
       {/* static */}
       <Row>
         <a href="#!" className="link">View More</a>
       </Row>
-    </div>
+    </ul>
   )
 }
 
--- src/pages/homepage/Links.js
+++ src/pages/homepage/Links.js
@@ -1,22 +1,37 @@
 import React from 'react'
 
 const Links = () => {
+    const links = [
+        {
+            title: 'Central Railway Website',
+            url: 'https://wcrms.com/'
+        },
+        {
+            title: 'Ministry of Electronics & Information Technology',
+            url: 'http://meity.gov.in/'
+        },
+        {
+            title: 'UIDAI',
+            url: 'http://www.uidai.gov.in/'
+        },
+        {
+            title: 'NTIPRIT',
+            url: 'https://www.ntiprit.gov.in/'
+        },
+        {
+            title: 'Bharat Broadband Network Limited',
+            url: 'http://www.bbnl.nic.in/'
+        },
+    ];
     return (
         <div className="Links">
             <p className="title">Important Links</p>
             <ul>
-                <li className="Link">
-                   <a href="" target="_blank" rel="noopener noreferrer">Central Railway Website</a>
-                </li>
-                <li className="Link">
-                   <a href="" target="_blank" rel="noopener noreferrer">Central Railway Website</a>
-                </li>
-                <li className="Link">
-                   <a href="" target="_blank" rel="noopener noreferrer">Central Railway Website</a>
-                </li>
-                <li className="Link">
-                   <a href="" target="_blank" rel="noopener noreferrer">Central Railway Website</a>
-                </li>
+                {links?.map((item, id) => (
+                    <li className="Link" key={id}>
+                        <a href={item.url} target="_blank" rel="noopener noreferrer">{item.title}</a>
+                    </li>
+                ))}
             </ul>
         </div>
     )

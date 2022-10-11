29c8ec3f1b832155b8b9fd98a90c6514e04ea688
devaraj764 <devaraju.edgroom.com>
2022-10-11 10:08:26 +0530

    Changes

--- src/pages/homepage/Carousel.js
+++ src/pages/homepage/Carousel.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { Carousel} from 'react-bootstrap';
+import { Carousel } from 'react-bootstrap';
 import First from '../../assets/carousel/1.png'
 
 const CarouselComponent = () => {
     return (
         <Carousel>
             <Carousel.Item interval={1000}>
-                <div className="screen">
-                    <img
-                        className="d-block w-100"
-                        src={First}
-                        alt="First slide"
-                    />
-                </div>
+                <img
+                    className="w-100"
+                    src={First}
+                    alt="First slide"
+                />
             </Carousel.Item>
         </Carousel>
     )

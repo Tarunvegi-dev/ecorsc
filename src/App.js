2cdfad94aea5b4bd9d725788a926d842f1a6fb47
devaraj764 <devaraju.edgroom.com>
2022-10-14 17:59:10 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Contact from './pages/Contact'
 import Admin from './pages/Admin/'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles.css';
+import Circulars from './pages/Circulars';
+import PNM_Items from './pages/PNM_Items';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <Switch>
         <Route path='/admin' component={Admin} />
         <Route path="/" exact component={Homepage} />
+        <Route path="/board-circulars" exact component={Circulars} />
+        <Route path="/pnm-items" exact component={PNM_Items} />
         <Route path="/history-and-affiliations" exact component={Affiliations} />
         <Route path='/central-office-bearers' component={CentralOfficeBearers} />
         <Route path='/contact' component={Contact} />
--- src/pages/homepage/Circulars.js
+++ src/pages/homepage/Circulars.js
@@ -7,37 +7,50 @@ const Circulars = () => {
     <div className="Cirsulars">
       <h3 className="title">Railway Board Circulars</h3>
       <Events />
+      <Row>
+        <a href="/board-circulars" className="link">View More</a>
+      </Row>
       <br />
       <h3 className="title">ECoRSC PNM Items</h3>
       <Events />
+      <Row>
+        <a href="/pnm-items" className="link">View More</a>
+      </Row>
     </div >
   )
 }
 
-const Events = () => {
+export const Events = () => {
   return (
     <ul className="table">
       <li className="item">
-        <p>29th National Convention of NFIR to be held on 17th & 18th September 2019 at Ujjain
+        <p>
+          <a>29th National Convention of NFIR to be held on 17th & 18th September 2019 at Ujjain
+          </a>
           <img src={NewGif} height="28px" alt="New Gif" />
         </p>
       </li>
       <li className="item">
-        <p>29th National Convention of NFIR to be held on 17th & 18th September 2019 at Ujjain</p>
-      </li>
-      <li className="item">
-        <p>29th National Convention of NFIR to be held on 17th & 18th September 2019 at Ujjain
+        <p>
+          <a>29th National Convention of NFIR to be held on 17th & 18th September 2019 at Ujjain
+          </a>
           <img src={NewGif} height="28px" alt="New Gif" />
         </p>
       </li>
       <li className="item">
-        <p>29th National Convention of NFIR to be held on 17th & 18th September 2019 at Ujjain</p>
+        <p>
+          <a>29th National Convention of NFIR to be held on 17th & 18th September 2019 at Ujjain
+          </a>
+          <img src={NewGif} height="28px" alt="New Gif" />
+        </p>
       </li>
-
-      {/* static */}
-      <Row>
-        <a href="#!" className="link">View More</a>
-      </Row>
+      <li className="item">
+        <p>
+          <a>29th National Convention of NFIR to be held on 17th & 18th September 2019 at Ujjain
+          </a>
+          <img src={NewGif} height="28px" alt="New Gif" />
+        </p>
+      </li>
     </ul>
   )
 }

bad5f0a451b2bd2e13e43990cb2f847a1f38801a
TarunBabu <vegitarun1234@gmail.com>
2022-10-23 08:58:05 +0530

    latest

--- src/pages/homepage/Links.js
+++ src/pages/homepage/Links.js
@@ -23,12 +23,87 @@ const Links = ({ title }) => {
             title: 'Bharat Broadband Network Limited',
             url: 'http://www.bbnl.nic.in/'
         },
+        {
+            title: 'Railway Board Circulars',
+            ur: 'https://railwayrule.com/railway-board-circulars'
+        },
+        {
+            title: 'NFIR',
+            url: 'https://www.nfirindia.com/'
+        },
+        {
+            title: 'Find Shortest Path',
+            url: 'https://rbs.indianrail.gov.in/ShortPath/ShortPath.jsp'
+        },
+        {
+            title: 'ECoRSC Zonal PNM Minutes',
+            url: 'https://eastcoastrail.indianrailways.gov.in/view_section.jsp?fontColor=black&backgroundColor=LIGHTSTEELBLUE&lang=0&id=0,6,1335,1336'
+        },
+        {
+            title: 'PNR Status',
+            url: 'https://www.indianrail.gov.in/enquiry/PNR/PnrEnquiry.html?locale=enF'
+        }
     ];
+
+    const otherLinks = [
+        {
+            title: 'Ministry of Railways',
+            url: 'https://indianrailways.gov.in/railwayboard/view_section.jsp?id=0,1,304,366,390,412,836'
+        },
+        {
+            title: 'AIRF',
+            url: 'http://www.airfindia.com/'
+        },
+        {
+            title: 'CGHS at a glance',
+            url: 'https://cghs.gov.in/CghsGovIn/faces/ViewPage.xhtml'
+        },
+        {
+            title: 'Indian Railways Website',
+            url: 'https://indianrailways.gov.in/'
+        },
+        {
+            title: 'HRMS Login',
+            url: 'https://hrms.indianrail.gov.in/HRMS/login'
+        },
+        {
+            title: 'UMID Login',
+            url: 'https://umid.digitalir.in/1_modules_dashboard/0_login'
+        },
+        {
+            title: 'Train Enquiry',
+            url: 'https://enquiry.indianrail.gov.in/ntes/'
+        },
+        {
+            title: 'Income Tax',
+            url: 'http://irtsa.net/IncomeTax.html'
+        },
+        {
+            title: 'Railway Board Directory',
+            url: 'http://dir.railnet.gov.in/'
+        },
+        {
+            title: 'Information Under RTI',
+            url: 'https://medlineplus.gov/tutorials/'
+        },
+        {
+            title: 'Pensioners Portal',
+            url: 'https://pensionersportal.gov.in/'
+        },
+        {
+            title: 'Government Websites',
+            url: 'https://www.irtsa.net/Government_Websites.html'
+        }
+    ]
     return (
         <div className="Links">
             <p className="title">{title}</p>
             <ul>
-                {links.map((item, id) => (
+                {title === 'Important Links' ? links.map((item, id) => (
+                    <li className="Link" key={id}>
+                        <FiChevronsRight style={{ color: 'gray' }} />&nbsp;<a href={item.url}>{item.title}</a>
+                    </li>
+                )) : otherLinks.map((item, id) => (
                     <li className="Link" key={id}>
                         <FiChevronsRight style={{ color: 'gray' }} />&nbsp;<a href={item.url}>{item.title}</a>
                     </li>
